72419f18186359efb7834a3d6c3a39bfe052ef88
Ashraf Sayed <ashraf.neizk@gmail.com>
2022-07-26 21:24:26 +0200

    services and some js fixes

--- JS/main.js
+++ JS/main.js
@@ -1,17 +1,54 @@
-
-
+// Global Vars
+const burgerIcon = document.getElementById("mobile-menu-icon");
+const menuContent = document.querySelector("header nav ul");
+const sections = document.querySelectorAll('section');
+const scrollTop = document.querySelector('.scroll-to-top')
+const fixedHeader = document.querySelector('.fixed-header');
+// Dynamicly creat nav
+function creatNavs(){
+    sections.forEach((e)=>{
+        let listItem = document.createElement('li');
+        let linkItem = document.createElement('span');
+        linkItem.innerHTML = e.dataset.nav;
+        menuContent.appendChild(listItem);
+        listItem.appendChild(linkItem);
+    })
+};
+creatNavs();
 let navList = document.querySelectorAll("ul.navs li");
-console.log(navList)
-navList.forEach(function(ele){
-    ele.onclick = function(){
-        navList.forEach(function(ele){
-            ele.classList.remove("active");
-        });
-        this.classList.add("active");
+// helper functions
+function removeActive(){
+    navList.forEach((e)=>{
+        e.classList.remove('active-nav')
+    })
+}
+// Events
+onscroll = function(){
+    for(let i=0;i<sections.length;i++){
+        if(window.scrollY>=sections[i].offsetTop - (sections[i].offsetHeight * 0.35)){
+            removeActive();
+            navList[i].classList.add('active-nav')
+        }
+    }
+    window.scrollY >= sections[0].offsetHeight * 0.85? scrollTop.style.right = "4%" : scrollTop.style.right = "-30%";
+    window.scrollY >= sections[0].offsetHeight * 0.75? fixedHeader.style.backgroundColor = "#666666cc" : fixedHeader.style.backgroundColor = "";
+}
+for(let i = 0;i < navList.length;i++){
+    navList[i].onclick = function(){
+        window.scrollTo({
+            top: sections[i].offsetTop,
+            left:0,
+            behavior: "smooth"
+        })
     };
-});
+};
+scrollTop.addEventListener('click',()=>{
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    })
+})
 let landingDots = document.querySelectorAll("span.background-dots span");
-console.log(landingDots)
 landingDots.forEach(function(ele){
     ele.onclick = function(){
         landingDots.forEach(function(ele){
@@ -21,13 +58,7 @@ landingDots.forEach(function(ele){
     };
 });
 // show menu in mobile 
-let burgerIcon = document.getElementById("mobile-menu-icon");
-let menuContent = document.querySelector("header nav ul");
 burgerIcon.onclick = function(){
-    menuContent.style.transform = "translateY(-55px)"
-}
-// close menu in mobile
-let closeIcon = document.getElementById("close-mobile-menu");
-closeIcon.onclick = function(){
-    menuContent.style.transform = "translateY(-205px)"
+    // menuContent.style.top = "-100%"?menuContent.style.top = "14 %":menuContent.style.top = "-100%";
+    menuContent.classList.toggle("opened-menu")
 }
